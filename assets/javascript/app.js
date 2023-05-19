065c19c95d004e74fb3a1cc06fb50df2ca6a5d93
jamariware <129812103+jamariware@users.noreply.github.com>
2023-05-19 16:45:51 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ class _app {
     backgroundToggler = false;
     shouldIgnoreVideo = false;
     effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-    brandDescription = ['#1 yeat fan', 'zurly#0001', 'ecrime', '💳', '#1 skid', '$$$', 'pc nerd', '.gg/bands'];
+    brandDescription = [ 'jamari#0001',  '$$$', 'feds.lol/bankroll'];
   
     titleChanger = (text, delay) => {
       if (!text) return;
